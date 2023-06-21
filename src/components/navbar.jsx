7dcdbb07c5ca4df949a249b43411410b3c3c1b6d
niklasrot <niklas.rothermund1@gmail.com>
2023-06-21 16:33:52 +0200

    Test

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,11 +1,13 @@
 import React from "react"
+import ReactLogo from "./React-icon.svg"
+
 
 export default function Navbar() {
     return (
         <div>
             <header>
                 <nav className="nav">
-                    <img src="./React-icon.svg" className="nav-logo" />
+                    <img src={ReactLogo} className="nav-logo" />
                     <h2 className="reactfacts">ReactFacts</h2>
                     <h2 className="reactcoursetext">React Course - Project 1</h2>
                 </nav> 
